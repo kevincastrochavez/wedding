b99e005efa0e59fe202a543dbe70c8ac1f3517fb
Kevin <kevin.castro.chavez0@gmail.com>
2023-02-13 23:38:57 -0700

    finished the footer along with some styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className='footer'>
+      <h2>REMINDER</h2>
+
+      <div className='footer__bar'></div>
+
+      <section className='footer_reminder'>
+        <h3>Party</h3>
+
+        <p>Saturday, March 25, 2023</p>
+        <p>Rexburg, ID. 6:00pm</p>
+      </section>
+
+      <section className='footer_reminder'>
+        <h3>Sealing</h3>
+
+        <p>Saturday, April 8, 2023</p>
+        <p>Mexico City, Mexico. 12:00pm</p>
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
