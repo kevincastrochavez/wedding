34a4886a5d6639e2d4ab1dc6c3a9e1b3c15d9756
Kevin <kevin.castro.chavez0@gmail.com>
2023-02-14 18:10:05 -0700

    created the gallery page along with some styles

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Fest from '../assets/fest.jpg';
+
 function Gallery() {
-  return <div>Gallery</div>;
+  return (
+    <main className='gallery'>
+      <div className='gallery__separator'></div>
+
+      <div className='gallery__container'>
+        <img src={Fest} alt='Picture' />
+        <img src={Fest} alt='Picture' />
+        <img src={Fest} alt='Picture' />
+        <img src={Fest} alt='Picture' />
+      </div>
+    </main>
+  );
 }
 
 export default Gallery;
