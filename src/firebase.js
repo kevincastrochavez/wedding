b48c0963f27295cc72ff06e54db3a0452b79632b
Kevin <kevin.castro.chavez0@gmail.com>
2023-02-14 18:29:27 -0700

    installed the firebase package and the config file

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA5zJWvzW6ZSdkxIe-p1vHrk5PeCVU0CH8',
+  authDomain: 'wedding-8b90a.firebaseapp.com',
+  projectId: 'wedding-8b90a',
+  storageBucket: 'wedding-8b90a.appspot.com',
+  messagingSenderId: '138062700860',
+  appId: '1:138062700860:web:29d7f4be37e606a7317e39',
+};
+
+const app = initializeApp(firebaseConfig);
